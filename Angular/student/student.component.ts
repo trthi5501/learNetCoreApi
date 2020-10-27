c83e2cd20d27b9f89ce3720679442df77bbd2863
trthi5501 <56109106+trthi5501@users.noreply.github.com>
2020-10-27 21:41:16 +0700

    first commit

---
+++ Angular/student/student.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor( private http:HttpClient) { }
+  values: any;
+
+  name='';
+
+  ngOnInit(){
+   return this.http.get("https://localhost:44349/api/Student").subscribe(response=>{
+     console.log(response);
+     this.values = response;
+   },error =>{
+     console.log(error);
+   });
+  
+   
+   
+  }
+  
+}
---
+++ AspCore/Model/StuentModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Model
+{
+    public class StuentModel
+    {
+        /// <summary>
+        /// Student ID
+        /// </summary>
+        public string SID { get; set; }
+        /// <summary>
+        /// Student SName
+        /// </summary>
+        public string SName { get; set; }
+        /// <summary>
+        /// Student Phone
+        /// </summary>
+        public string SPhone { get; set; }
+        /// <summary>
+        /// Student Gender
+        /// </summary>
+        public string SGender { get; set; }
+        /// <summary>
+        /// Student Address
+        /// </summary>
+        public string SAddress { get; set; }
+
+        public static List<StuentModel> StudentList = new List<StuentModel>();
+
+        public List<StuentModel> GetStuents()
+        {
+            var students = new List<StuentModel>();
+            //string connect
+            string connectString = @"Server=DESKTOP-7I2D4KB\SQLEXPRESS;Database=TEST;Trusted_Connection=True;";
+            // SqlConnection object initialization
+            SqlConnection sqlConnection = new SqlConnection(connectString);
+            //sql Command  use stored produre
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.CommandText = "dbo.Proc_GetStudent";
+            //connect database
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                var student = new StuentModel();
+                for(int i = 0; i<sqlDataReader.FieldCount; i++)
+                {
+                    //lấy tên và  giá trị
+                    var colName = sqlDataReader.GetName(i);
+                    var value = sqlDataReader.GetValue(i);
+                    // mapping model
+                    var property = student.GetType().GetProperty(colName);
+                    if (property != null)
+                    {
+                        property.SetValue(student, value);
+                    }
+                }
+                //thêm đối tượng vào list
+                students.Add(student);
+            }
+            //close Connection
+            sqlConnection.Close();
+
+            return students;
+
+        }
+
+    }
+}
